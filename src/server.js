845d60305e5196f26ed43bf4d40f803f2779211f
Pablo Augusto <augusto12d3@gmail.com>
2020-06-05 15:44:39 -0300

    nunjuck
    organizando pastas e server

--- src/server.js
+++ src/server.js
@@ -1,12 +1,31 @@
 const express = require("express")
 const server = express()
 
+// configurar pasta publica
+server.use(express.static("public"))
+
+
+// utilizando template engine
+const nunjucks = require("nunjucks")
+nunjucks.configure("src/views", {
+    express: server,
+    noCache: true
+})
+
 
 //configurar caminhos da aplicação
 //pagina inicial
 //req: requisição  res: resposta
 server.get("/", (req, res) => {
-    res.sendFile(__dirname + "/views/index.html")
+    return res.render("index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    return res.render("create-point.html")
+})
+
+server.get("/search", (req, res) => {
+    return res.render("search-results.html")
 })
 
 
