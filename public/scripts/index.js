22def4d1efc138cd5a1521e34ef33b53540a8b16
Pablo Augusto <augusto12d3@gmail.com>
2020-06-07 05:34:29 -0300

    Projeto finalizado

--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,10 +1,8 @@
 const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
-
 buttonSearch.addEventListener("click", () => {
-    modal.classList.remove("hide")
-    
+    modal.classList.toggle("hide")
 })
 
 close.addEventListener("click", () => {
